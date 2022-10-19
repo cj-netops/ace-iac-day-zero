c4072e86183c63ceeaaf92a7520139dec6d9e70d
cj-netops <108844246+cj-netops@users.noreply.github.com>
2022-10-18 23:43:05 -0700

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@ terraform {
   required_version = ">= 1.1.0"
   backend "remote" {
     hostname = "app.terraform.io"
-    # organization = "<replace-with-your-Terraform-Cloud-organization-and-uncomment>"
+    organization = "TF-DEV-AVX"
     workspaces {
       name = "ace-iac-day-zero"
     }
